d0d088b06832f0e3293a25e7775bbb656893f21a
Adele Apsite <adeleapsite@gmail.com>
2024-04-06 11:37:09 +0300

    Create a migration and database model for posts

---
+++ app/Models/Post.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['title', 'link', 'points', 'deleted_by'];
+
+    public function deletedBy()
+    {
+        return $this->belongsTo(User::class, 'deleted_by');
+    }
+}
