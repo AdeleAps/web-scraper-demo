d79570ff4036405bbd0e4a6381b88e4114affaeb
Adele Apsite <adeleapsite@gmail.com>
2024-04-07 17:14:20 +0300

    Add styling for data table

--- resources/js/Components/DataTable.vue
+++ resources/js/Components/DataTable.vue
@@ -10,24 +10,37 @@ const props = defineProps({
 DataTable.use(DataTablesCore);
 
 const columns = [
-    { data: "points", title: "Points" },
-    { data: "title", title: "Title" },
-    { data: "link", title: "Link" },
+    // {
+    //     data: null,
+    //     className: "dt-center editor-edit",
+    //     defaultContent: '<button><i class="fa fa-pencil"/>hello</button>',
+    //     orderable: false,
+    // },
+    {
+        data: "title",
+        title: "Title",
+        render: (data, type, row) =>
+            `<a href="${row.link}" target="_blank" rel="noopener noreferrer">${data}</a>`,
+    },
     { data: "origin_date", title: "Date" },
 ];
 
 const options = {
     responsive: true,
+    scrollY: "50vh",
 };
 </script>
 
+<style>
+@import "datatables.net-dt";
+</style>
+
 <template>
     <div>
-        <div style="min-height: 78vh" class="bg-main-light p-6 lg:p-8">
+        <div class="bg-main-light p-6 lg:p-8">
             <DataTable
                 :columns="columns"
                 :data="props.posts"
-                class="display"
                 :options="options"
             >
             </DataTable>
