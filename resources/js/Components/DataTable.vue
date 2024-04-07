486fb3f64b028e81dc8d7744cdd81faad26f8004
Adele Apsite <adeleapsite@gmail.com>
2024-04-07 19:02:48 +0300

    Add delete button for data table on frontend

--- resources/js/Components/DataTable.vue
+++ resources/js/Components/DataTable.vue
@@ -2,20 +2,41 @@
 import DataTable from "datatables.net-vue3";
 import DataTablesCore from "datatables.net";
 import "datatables.net-responsive";
+import { onMounted, ref } from "vue";
 
 const props = defineProps({
     posts: Array,
 });
 
+const dataTableRef = ref(null);
+
 DataTable.use(DataTablesCore);
 
+const deleteMe = (param) => {
+    console.log(param);
+};
+
+// This is clunky, but the DataTable disables Vue's events so I have to attach event listeners manually and leverage
+// data attributes.
+
+onMounted(() => {
+    dataTableRef.value.$el.addEventListener("click", (event) => {
+        const target = event.target;
+        if (target.classList.contains("fa-trash")) {
+            const rowId = target.getAttribute("data-row-id");
+            deleteMe(rowId);
+        }
+    });
+});
+
 const columns = [
-    // {
-    //     data: null,
-    //     className: "dt-center editor-edit",
-    //     defaultContent: '<button><i class="fa fa-pencil"/>hello</button>',
-    //     orderable: false,
-    // },
+    {
+        data: null,
+        render: (data, type, row) =>
+            `<button aria-label="Delete"><i class="fa fa-trash fa-lg" data-row-id="${row.id}"/></button>`,
+        orderable: false,
+    },
+    { data: "points", title: "Points", className: "dt-body-center" },
     {
         data: "title",
         title: "Title",
@@ -39,6 +60,8 @@ const options = {
     <div>
         <div class="bg-main-light p-6 lg:p-8">
             <DataTable
+                ref="dataTableRef"
+                class="stripe"
                 :columns="columns"
                 :data="props.posts"
                 :options="options"
---
+++ resources/js/Components/DeleteButton.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    methods: {
+        callback: function (e) {
+            this.$emit("click", e);
+        },
+    },
+};
+</script>
+ƒ
+<template>
+    <button @click="callback($event)">delete</button>
+</template>
